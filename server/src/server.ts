dadd0772bd5378f5a60edbbac1e84b98c129c0df
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-24 14:11:48 -0500

    revised server.ts code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,55 +1,30 @@
 import express from 'express';
 import path from 'node:path';
-import type { Request, Response } from 'express';
-// Import the ApolloServer class
-import {
-  ApolloServer,
-} from '@apollo/server';
-import {
-  expressMiddleware
-} from '@apollo/server/express4';
-import { authenticateToken } from './services/auth-service.js';
-// Import the two parts of a GraphQL schema
-import { typeDefs, resolvers } from './schemas/index.js';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import db from './config/connection.js';
 
 
-const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async () => {
-  await server.start();
-  await db;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server as any,
-    {
-      context: authenticateToken as any
-    }
-  ));
+// if we're in production, serve client/build as static assets and ensure the index.html file is served for the React Router to handle UI views
+if (process.env.PORT) {
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+  })
+}
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
-
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-  });
-
-};
-
-// Call the async function to start the server
-startApolloServer();
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
